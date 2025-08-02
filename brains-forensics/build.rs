5f9e9b3b6ab77ed8f6d2564aea8c382c504bad2b
Johhny Monotonic <jonny.monotonic@gmail.com>
2025-08-02 14:15:35 +1200

    forensics/build: inline format args in println! to satisfy clippy::uninlined-format-args (fixes #12)

--- brains-forensics/build.rs
+++ brains-forensics/build.rs
@@ -4,6 +4,6 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     
     if let Ok(ver) = version() {
-        println!("cargo:rustc-env=RUSTC_VERSION={}", ver);
+        println!("cargo:rustc-env=RUSTC_VERSION={ver}");
     }
 }
